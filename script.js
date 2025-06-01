2fb8e3a277317c9bbe7736834a25ef2e0831848a
54aaron <arj2145@columbia.edu>
2025-05-31 21:48:21 -0400

    updates

---
+++ script.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+
+    overlayStartUp();
+    
+    $("#currently-reading-overlay").on("click", function() { 
+        $("#currently-reading").hide();
+    })
+
+    $("#info").on("click", function() { 
+        $("#currently-reading").show();
+    })
+});
+
+
+function overlayStartUp() {
+    $("#currently-reading").show();
+    $("#currently-reading-overlay").show();
+
+    setTimeout(function() {
+        $("#currently-reading-loading-bar").hide();
+        $("#currently-reading-loading-bar").css("marginLeft", "35%")
+    }, 500);
+    
+    setTimeout(function() {
+        $("#currently-reading-loading-bar").show();
+    }, 1000); 
+
+    setTimeout(function() {
+        $("#currently-reading-loading-bar").hide();
+        $("#currently-reading-loading-bar").css("marginLeft", "65%")
+    }, 1500);
+
+    setTimeout(function() {
+        $("#currently-reading-loading-bar").show();
+    }, 2000);
+    
+    setTimeout(function() {
+        // $("#currently-reading-loading-bar").hide();
+        $("#currently-reading").hide();
+        $("#currently-reading-loading-bar").css("marginLeft", "0%")
+    }, 3000);
+}
